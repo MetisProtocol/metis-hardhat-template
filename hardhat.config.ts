650868e3b37b4f3bd791a88a8e6e042653d789dc
Eric <92196970+ericlee42@users.noreply.github.com>
2022-11-03 22:58:47 +0800

    remove stardust and add metis goerli (#3)
    * remove stardust and add metis goerli
    
    * fix ci
    
    * fix ci failure

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,49 +1,45 @@
-import * as dotenv from "dotenv";
-
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
-import "./ts-src/scripts/accounts";
 
+import * as dotenv from "dotenv";
 dotenv.config();
 
+import "./ts-src/scripts/accounts";
+
+const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 if (!process.env.PRIVATE_KEY) {
   throw new Error("No private key");
 }
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.4",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
         runs: 800,
       },
-      // There is a bug of explorer here
-      // If you use this, you won't be able to verify your contract
-      // metadata: {
-      //   bytecodeHash: "none",
-      // },
+      evmVersion: "berlin",
+      metadata: {
+        bytecodeHash: "none",
+      },
     },
   },
   networks: {
-    stardust: {
-      url: "https://stardust.metis.io/?owner=588",
-      accounts: [process.env.PRIVATE_KEY],
+    metisgoerli: {
+      url: "https://goerli.gateway.metisdevops.link",
+      accounts: [WALLET_PRIVATE_KEY],
       verify: {
         etherscan: {
-          // just use api-key
           apiKey: "api-key",
-          apiUrl: "https://stardust-explorer.metis.io",
+          apiUrl: "https://goerli.explorer.metisdevops.link",
         },
       },
     },
     andromeda: {
       url: "https://andromeda.metis.io/?owner=1088",
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [WALLET_PRIVATE_KEY],
       verify: {
         etherscan: {
           // just use api-key
@@ -55,10 +51,6 @@ const config: HardhatUserConfig = {
   },
   namedAccounts: {
     deployer: 0,
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
   },
   paths: {
     tests: "ts-src/test",
